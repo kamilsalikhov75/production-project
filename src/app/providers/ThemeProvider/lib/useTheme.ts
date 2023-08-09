f5fe8af9a46a233bfc566a3fe5a74a9394786273
kasmil <kamilsalikhov75@gmail.com>
2023-08-09 23:15:55 +0300

    added eslint

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -1,18 +1,18 @@
-import { useContext } from "react";
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "app/providers/ThemeProvider/lib/ThemeContext";
+import { useContext } from 'react';
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from 'app/providers/ThemeProvider/lib/ThemeContext';
 
 export interface UseThemeResult {
   toggleTheme: () => void;
   theme: Theme;
 }
 export function useTheme(): UseThemeResult {
-  const { theme, setTheme } = useContext(ThemeContext);
+    const { theme, setTheme } = useContext(ThemeContext);
 
-  const toggleTheme = () => {
-    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
-    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
-    setTheme(newTheme);
-  };
+    const toggleTheme = () => {
+        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+        setTheme(newTheme);
+    };
 
-  return { theme, toggleTheme };
+    return { theme, toggleTheme };
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import { render } from "react-dom";
-import { App } from "./app/App";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "app/providers/ThemeProvider";
-import "./shared/config/i18next/i18next";
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'app/providers/ThemeProvider';
+import { App } from './app/App';
+import './shared/config/i18next/i18next';
 
 render(
-  <BrowserRouter>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>,
-  document.getElementById("root")
+    <BrowserRouter>
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    </BrowserRouter>,
+    document.getElementById('root'),
 );
